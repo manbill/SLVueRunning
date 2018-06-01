d68defa6eb3354da29492d301f15d56fa47b7599
manbiao.huang <570607278@qq.com>
2018-06-01 19:25:15 +0800

    标题设置显示隐藏

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id='app'>
     <mu-appbar color="primary" v-if='appBar.height'>
-          <mu-button icon slot="left">
-            <mu-icon value="keyboard_arrow_left" @click="goback(-1)"></mu-icon>
+          <mu-button icon slot="left" @click="goback()">
+            <mu-icon value="keyboard_arrow_left"></mu-icon>
           </mu-button>
           Photo
     </mu-appbar>
@@ -23,8 +23,12 @@ export default {
     }
   },
   methods:{
-    goback(n){
-      this.$route.goback(n);
+    goback(){
+      if(this.$route.matched.length>1){
+        this.$router.back();
+      }else{
+        console.log(this.$route.matched.length);
+      }
     },
     
   },
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,10 +4,11 @@
                 <mu-button icon >
                     <mu-icon value="keyboard_arrow_left" ></mu-icon>
                 </mu-button>
-                <mu-flex class='carousel-image' justify-content='center'>
+                <mu-flex class='carousel-image' direction='column' justify-content='center'>
                     <mu-paper :z-depth="1">
-
+                    
                     </mu-paper>
+
                 </mu-flex>
                 <mu-button  icon >
                 <mu-icon value="keyboard_arrow_right" ></mu-icon>
@@ -31,44 +32,50 @@
     </mu-flex>
 </template>
 <script>
-    import { mapGetters } from "vuex";
-    export default {
-        props:['height'],
-        data(){
-            return {
-                home:"首页",
-                styleObj:{
-                    height:(this.appBar.show?window.innerHeight-this.appBar.height:window.innerHeight)+"px"
-                }
-            }
-        },
-        name:'home',
-        computed:{
-           ...mapGetters(['appBar'])
-        }
-    }
+import { mapGetters } from "vuex";
+export default {
+  name: "home",
+  props: ["height"],
+  data() {
+    return {
+      home: "首页",
+      styleObj: {}
+    };
+  },
+  created(){
+      this.styleObj = {
+        height:
+          (this.appBar.show
+            ? window.innerHeight - this.appBar.height
+            : window.innerHeight) + "px"
+      }
+  },
+  computed: {
+    ...mapGetters(["appBar"])
+  }
+};
 </script>
 <style lang='scss' scoped>
-    .flex-wrapper{
-        width: 100%;
-        .carousel{
-            width: inherit;
-             height: 40%;
-        }
-        .grid-buttons{
-            width: inherit;
-            height: 60%;
-        }
-        .carousel{
-            background: skyblue;
-            .carousel-image{
-                background: tomato;
-                height: 100%;
-                flex: 10 1 0 ;
-            }
-        }
-        .grid-buttons{
-            background: khaki;
-        }
+.flex-wrapper {
+  width: 100%;
+  .carousel {
+    width: inherit;
+    height: 50%;
+  }
+  .grid-buttons {
+    width: inherit;
+    height: 50%;
+  }
+  .carousel {
+    background: skyblue;
+    .carousel-image {
+      background: tomato;
+      height: 80%;
+      flex: 10 1 0;
     }
+  }
+  .grid-buttons {
+    background: khaki;
+  }
+}
 </style>
